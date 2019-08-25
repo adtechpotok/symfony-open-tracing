9331d3fcd526f448d5e05a37c4487ed73ed8247f
Filip Benco <filip.benco@websupport.sk>
2019-08-25 23:59:02 +0200

    Add Configuration class

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace Adtechpotok\Bundle\SymfonyOpenTracing\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+
+    /**
+     * Generates the configuration tree builder.
+     *
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('open_tracing');
+        /** @var ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
+
+        // Basic Sentry configuration
+        $rootNode
+            ->children()
+                ->scalarNode('service_name')->end()
+                ->scalarNode('enabled')->defaultTrue()->end()
+                ->arrayNode('tracer_config')
+                    ->children()
+                        ->arrayNode('sampler')
+                            ->children()
+                                ->scalarNode('type')->defaultValue('const')->end()
+                                ->scalarNode('param')->defaultTrue()->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('local_agent')
+                            ->children()
+                                ->scalarNode('reporting_host')->defaultNull()->end()
+                                ->scalarNode('reporting_port')->defaultNull()->end()
+                            ->end()
+                        ->end()
+                        ->scalarNode('logging')->defaultTrue()->end()
+                        ->scalarNode('trace_id_header')->defaultNull()->end()
+                        ->scalarNode('baggage_header_prefix')->defaultNull()->end()
+                        ->scalarNode('debug_id_header_key')->defaultNull()->end()
+                    ->end()
+                ->end()
+            ->end();
+    }
+}
--- src/DependencyInjection/SymfonyOpenTracingExtension.php
+++ src/DependencyInjection/SymfonyOpenTracingExtension.php
@@ -7,17 +7,16 @@ namespace Adtechpotok\Bundle\SymfonyOpenTracing\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
-class SymfonyOpenTracingExtension extends Extension
+class SymfonyOpenTracingExtension extends ConfigurableExtension
 {
     /**
-     * {@inheritdoc}
+     * Configures the passed container according to the merged configuration.
      */
-    public function load(array $configs, ContainerBuilder $container)
+    protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resource/config'));
         $loader->load('services.yml');
-        $loader->load('parameters.yml');
     }
 }
